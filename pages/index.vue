60d13eae00a45f559a6cfe1fe13588faab752984
”taro <groveleffect@gmail.com>
2020-11-08 14:23:37 +0900

    Add Search

--- pages/index.vue
+++ pages/index.vue
@@ -2,6 +2,11 @@
 <template>
   <section class="container">
     <h1>Todoリスト</h1>
+    <button class="button button--gray" v-bind:class="{'is-active':(!find_flg)}" @click="flag_reset">全て</button>
+    <button class="button button--gray" v-bind:class="{'is-active':find_flg && (find_state == '作業前')}" @click="find('作業前')">作業前</button>
+    <button class="button button--gray" v-bind:class="{'is-active':find_flg && (find_state == '作業中')}" @click="find('作業中')">作業中</button>
+    <button class="button button--gray" v-bind:class="{'is-active':find_flg && (find_state == '完了')}" @click="find('完了')">完了</button>
+   Xhtml
     <div class="addArea">
       <input type="text" name="addName" v-model="content" placeholder="タスクを入力してください">
      <button class="button button--green" @click="insert">追加</button>
@@ -22,7 +27,7 @@
         </tr>
       </thead>
       <tbody>
-        <tr v-for="(item, index) in todos" :key="index">
+        <tr v-for="(item, index) in display_todos" :key="index">
           <td>{{ item.content }}</td>
           <td>{{ item.created }}</td>
           <td>
@@ -49,11 +54,27 @@ import {mapState} from 'vuex';
 export default {
   data: function() {
     return {
-      content: ''
+      content: '',
+      find_state: '',
+      find_flg: false
     }
   },
   computed: {
-    ...mapState(['todos'])
+    ...mapState(['todos']),
+    display_todos:function(){
+      if(this.find_flg){
+        let arr = [];
+        let data = this.todos;
+        data.forEach(element =>{
+          if(element.state == this.find_state){
+            arr.push(element);
+          }
+        });
+        return arr;
+      }else{
+        return this.todos;
+      }
+    }
   },
   methods: {
     insert: function() {
@@ -67,6 +88,13 @@ export default {
     },
     changeState: function(todo){
       this.$store.commit('changeState',todo)
+    },
+    find: function(findState){
+      this.find_state = findState;
+      this.find_flg = true;
+    },
+    flag_reset: function(){
+     this.find_flg = false;
     }
   }
 }
